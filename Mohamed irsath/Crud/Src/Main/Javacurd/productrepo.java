c4225953994f1d8134b64b5383da8535ba2c8ee5
irsatharsi <71000925+irsatharsi@users.noreply.github.com>
2020-09-09 03:03:30 -0700

    Add files via upload

---
+++ Mohamed irsath/Crud/Src/Main/Javacurd/productrepo.java
@@ -0,0 +1,10 @@
+Package com.irsath.crud.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+ 
+
+public class ProductRepository implements JpaRepository<Product , Integer> {
+
+ 
+
+}
