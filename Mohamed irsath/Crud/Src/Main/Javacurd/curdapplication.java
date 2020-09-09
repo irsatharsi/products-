c470d92d5d3af01ddfdd268d6ff1a6fe29dde0c0
irsatharsi <71000925+irsatharsi@users.noreply.github.com>
2020-09-09 03:06:05 -0700

    Add files via upload

---
+++ Mohamed irsath/Crud/Src/Main/Javacurd/curdapplication.java
@@ -0,0 +1,25 @@
+package com.irsath.crud;
+
+ 
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+ 
+
+@SpringBootApplication
+
+public class IrsathApplication {
+
+ 
+
+                public static void main(String[] args) {
+
+                                SpringApplication.run(IrsathApplication.class, args);
+
+                }
+
+ 
+
+}
